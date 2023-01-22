ffe065340f6a4d88e380f2ba06f219875064e990
Chrisld50 <chrisld50@yahoo.com>
2023-01-21 23:14:12 -0600

    added json files, imported apollo on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,39 @@
 import React, { Profile } from "react";
 import { Container } from "react-bootstrap";
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-
-import ApolloProvider from "./ApolloProvider";
-
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
 import "./App.scss";
 
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-import Profile from "./pages/Profile";
-import Messages from "./pages/Messages"
+import Messages from "./pages/Messages";
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <ApolloProvider>
+    <ApolloProvider client = {client}>
       <BrowserRouter>
     <Container className="pt-5">
       <Switch>
