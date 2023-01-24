6be81ea00ffcf82ea1c1c435ba590e99dbafc2bc
shawn220a <shawn.42496@gmail.com>
2023-01-24 01:05:45 -0600

    Logout button removes jwt token

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,29 +1,29 @@
 import React from 'react';
-// import { Row, Col, Button } from 'react-bootstrap';
-// import { Link } from 'react-router-dom';
+import { Row, Col, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Auth from '../utils/auth';
 
+export default function Home() {
 
-// import { useAuthDispatch } from '../context/auth'
-
-export default function Home({ history }) {
-  //  const dispatch = useAuthDispatch()
-
-  //   const logout = () => {
-  //      dispatch({ type: 'LOGOUT' })
-  //       history.push('/login')
-  //  }
-   return (
+    const logout = () => {
+      Auth.logout()
+      // history.push('/login')
+   }
+  return (
     <div>
       <h1>Hello. You are logged in</h1>
+
+      <Row className='bg-white justify content-around'>
+        <Link to='/login'>
+          <Button variant='link'>Login</Button>
+        </Link>
+        <Link to='/register'>
+          <Button variant='link'>Register</Button>
+        </Link>
+        <Button variant='link' onClick={logout}>
+          Logout
+        </Button>
+      </Row>
     </div>
-      // <Row className="bg-white justify content-around">
-      //  <Link to="/login">
-      //      <Button variant="link">Login</Button>
-      //   </Link>
-      //   <Link to="/register">
-      //      <Button variant="link">Register</Button>
-      //  </Link>
-      //    <Button variant="link" onClick={logout}>Logout</Button>
-      //  </Row>
-   )
+  );
 }
