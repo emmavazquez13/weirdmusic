5d2a4c661717b598f6bb51f7e2a40777be360dbe
shawn220a <shawn.42496@gmail.com>
2023-01-24 23:36:49 -0600

    Functions work in the jsx file

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
-import { Container } from "react-bootstrap";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from "@apollo/client";
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 // import Home from './pages/Home'
-import Home from './components/home'
-import Register from "./pages/Register";
-import Login from "./pages/Login";
+import Home from './components/home';
+import Register from './pages/Register';
+// import Login from "./pages/Login";
+import Login from './components/login';
 import Profile from './pages/Profile';
 
 const httpLink = createHttpLink({
@@ -26,7 +32,6 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
-
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
@@ -34,27 +39,24 @@ const client = new ApolloClient({
 
 function App() {
   return (
-    <ApolloProvider client = {client}>
-    <Container className="pt-5">
+    <ApolloProvider client={client}>
+      <Container className='pt-5'>
+        <Router>
+          <>
+            <Routes>
+              <Route exact path='/' element={<Home />} />
 
-      <Router>
-        <>
-        <Routes>
-      <Route exact path="/" element={<Home />} />
+              <Route exact path='/register' element={<Register />} />
 
-      <Route exact path="/register" element={<Register />} />
+              <Route path='/login' element={<Login />} />
 
-      <Route path="/login" element={<Login />} />
-
-      <Route path="/profile" element={<Profile />} />
-      
-      </Routes>
-      </>
-      </Router>
-
-    </Container>
+              <Route path='/profile' element={<Profile />} />
+            </Routes>
+          </>
+        </Router>
+      </Container>
     </ApolloProvider>
-  )
+  );
 }
 
 export default App;
