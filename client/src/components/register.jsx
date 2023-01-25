6917fa76f888fbc8c47b7342cbee6db2e24c96aa
shawn220a <shawn.42496@gmail.com>
2023-01-25 01:33:09 -0600

    Remove console log

--- client/src/components/register.jsx
+++ client/src/components/register.jsx
@@ -34,7 +34,6 @@ export default function Register() {
     event.preventDefault();
 
     const form = event.currentTarget;
-    console.log(userFormData);
 
     if (form.checkValidity() === false) {
       event.preventDefault();
