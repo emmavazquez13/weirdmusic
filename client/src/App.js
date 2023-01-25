317d066d1a7a488b079ff2d381852faa7b0c7fec
DBEdelson <David.Edelson@outlook.com>
2023-01-24 21:20:59 -0600

    updated Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,11 @@
-import React from "react";
-import { Container } from "react-bootstrap";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from "@apollo/client";
-import { setContext } from '@apollo/client/link/context';
-
-import Home from './pages/Home'
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Messages from "./pages/Messages";
-import Profile from './pages/Profile';
-
-const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
-});
-
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+import './App.css';
 
 function App() {
   return (
-    <ApolloProvider client = {client}>
-    <Container className="pt-5">
-
-      <Router>
-        <>
-        <Routes>
-      <Route exact path="/" element={<Home />} />
-
-      <Route exact path="/register" element={<Register />} />
-
-      <Route path="/login" element={<Login />} />
-
-      <Route path="/profile" element={<Profile />} />
-      
-      <Route path="/messages" element={<Messages />} />
-      </Routes>
-      </>
-      </Router>
-
-    </Container>
-    </ApolloProvider>
-  )
+  <div>
+    <h1>hello react</h1>
+  </div>
+  );
 }
 
 export default App;
